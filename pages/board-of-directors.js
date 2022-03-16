5b79dac8bec5b0d3049bad93bb37a634f490e854
Ahmed <ahmaat19@gmail.com>
2022-03-16 20:17:31 +0300

    updating content

---
+++ pages/board-of-directors.js
@@ -0,0 +1,67 @@
+import Meta from '../components/Meta'
+import Navigation from '../components/Navigation'
+
+const BorderOfDirectors = () => {
+  const carousel = [
+    {
+      _id: 1,
+      title: 'BOARD OF DIRECTORS',
+      image: '/slide.jpg',
+      description:
+        'The Board of Directors (BOD) are the fiduciaries who steer the organization towards a sustainable future',
+      height: '540px',
+    },
+  ]
+  return (
+    <>
+      <Meta
+        title='Board Of Directors'
+        description='The Board of Directors (BOD) are the fiduciaries who steer the
+        organization towards a sustainable future by adopting sound,
+        ethical, and legal governance and financial management policies,
+        as well as making sure the organization has adequate resources to
+        advance its mission and attain its vision.'
+      />
+      <Navigation carousel={carousel} />
+
+      <div className='container py-3'>
+        <div className='row gy-3 my-3'>
+          <div className='col-lg-8 col-md-10 col-12 mx-auto text-center'>
+            <p>
+              The Board of Directors (BOD) are the fiduciaries who steer the
+              organization towards a sustainable future by adopting sound,
+              ethical, and legal governance and financial management policies,
+              as well as making sure the organization has adequate resources to
+              advance its mission and attain its vision.
+            </p>
+            <p>
+              SADO has board members who provide effective oversight and policy
+              direction of the organization as well as overall responsibilities
+              for ensuring accountability, credibility, integrity,
+              sustainability and representation. The board members are currently
+              composed of five (5) members 4 men and 1 woman with varied
+              professional backgrounds and representing diverse interests such
+              as gender, geographical and special interest group representation.
+            </p>
+            <p>
+              The board membership also has a number of expert committees such
+              as administration and Human Resources, finance and programs that
+              undertake various specialized roles. In order to effectively
+              direct the affairs of the organization, the board meets at least
+              four times in a year. The board of directors will set the{' '}
+              {"organization's"} strategic direction, construct a policy-based
+              governance framework, regulate the organization and its connection
+              with the Executive Director. In this governance structure, the BoD
+              shall provide oversight and accountability. The board of directors
+              shall guide and oversee the implementation of the{' '}
+              {" organization's "}
+              strategic plan
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default BorderOfDirectors
---
+++ pages/management-and-organization-structure.js
@@ -0,0 +1,89 @@
+import Meta from '../components/Meta'
+import Navigation from '../components/Navigation'
+
+const ManagementStructure = () => {
+  const carousel = [
+    {
+      _id: 1,
+      title: 'M. O. STRUCTURE',
+      image: '/slide.jpg',
+      description:
+        'The day to day management affairs of SADO are overseen by a management team whose membership comprises',
+      height: '540px',
+    },
+  ]
+
+  const list = [
+    {
+      _id: 1,
+      content:
+        'Study social problems affecting communities living within the project areas and design appropriate humanitarian response and development programs to address identified needs.',
+    },
+    {
+      _id: 2,
+      content:
+        'Oversee the planning, implementation, monitoring and evaluation of program activities and the preparation of relevant reports or document for the Board and Donors.',
+    },
+    {
+      _id: 3,
+      content: 'Coordinate the preparation of strategic and or annual plans.',
+    },
+    {
+      _id: 4,
+      content: 'Oversee the care for and management of SADO resources.',
+    },
+    {
+      _id: 5,
+      content:
+        'Ensure sufficient internal organizational management practice and capacity.',
+    },
+    {
+      _id: 6,
+      content:
+        'Harmonize/coordinate development activities in the field to ensure set results are met.',
+    },
+    {
+      _id: 7,
+      content:
+        'Advice the board of directors on policy formulation and/ or implementation.',
+    },
+  ]
+  return (
+    <>
+      <Meta
+        title='Management & Organization Structure'
+        description='The day to day management affairs of SADO are overseen by a management team whose membership comprises the Executive Director supported by Coordinators in Programs Administration and Human Resources and Finance. Below the coordinators are the program managers, field officers and various support functions.'
+      />
+      <Navigation carousel={carousel} />
+
+      <div className='container py-3'>
+        <div className='row gy-3 my-3'>
+          <div className='col-lg-8 col-md-10 col-12 mx-auto'>
+            <p>
+              The day to day management affairs of SADO are overseen by a
+              management team whose membership comprises the Executive Director
+              supported by Coordinators in Programs Administration and Human
+              Resources and Finance. Below the coordinators are the program
+              managers, field officers and various support functions. The team
+              has the option of co-opting other members if necessary. The role
+              of the management team includes;
+            </p>
+
+            <ol
+              className='list-group list-group-numbered '
+              style={{ marginLeft: '-15px' }}
+            >
+              {list.map((l) => (
+                <li key={l._id} className='list-group-item border-0'>
+                  {l.content}
+                </li>
+              ))}
+            </ol>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default ManagementStructure
